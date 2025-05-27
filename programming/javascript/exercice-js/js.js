1069ce116930e5c27e5134a5bbe2f3ba3cc29777
xavier <xav.dx02@gmail.com>
2025-05-27 14:25:42 +0000

    exo js

---
+++ programming/javascript/exercice-js/js.js
@@ -0,0 +1,45 @@
+let age1=4;
+let age2=6;
+let age3=(age1*age2)
+
+
+let prenom="john"
+let nom=" Doe"
+let nomComplet= prenom+nom
+console.log(nomComplet)
+
+let sujet="le chat"
+let verbe=" mange"
+let objet=" la souris"
+let phrase=sujet+verbe+objet
+console.log(phrase)
+
+let b="bonjour"
+let a="alice"
+function saluer(b, a){
+return  b + a ;
+}
+let resultat =b + a 
+console.log(resultat);
+
+
+let c=7;
+let d=8;
+function multiplication(c, d){
+return c * d;
+}
+let resulta = multiplication(c, d);
+console.log(resulta);
+
+let tab = [1, 2, 3, 4, 5];
+tab [0][0]
+tab [0][1]
+tab [0][2]
+tab [0][3]
+tab [0][4]
+
+let tabl= [];
+tabl.push ("10","20","30");
+console.log(tabl);
+tabl.shift;
+console.log(tabl);
