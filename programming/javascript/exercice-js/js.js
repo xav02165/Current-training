8f597bce99c6eaaf6a3ef784525e2585fc3313d9
xavier <xav.dx02@gmail.com>
2025-05-28 09:41:39 +0000

    jsmatin

--- programming/javascript/exercice-js/js.js
+++ programming/javascript/exercice-js/js.js
@@ -1,4 +1,4 @@
-let age1=4;
+/*let age1=4;
 let age2=6;
 let age3=(age1*age2)
 
@@ -41,5 +41,82 @@ tab [0][4]
 let tabl= [];
 tabl.push ("10","20","30");
 console.log(tabl);
-tabl.shift;
+
+
+let tabl= [];
+tabl.push ("10","20","30");
+tabl.shift ("10");
 console.log(tabl);
+
+let personne = {
+	nom: 'alice',
+	age: 25,
+	ville: 'Paris' 
+	};
+    console.log(personne);
+
+    let compte = {
+        solde: 1000,
+        titulaire: 'john doe',
+    };
+    compte.solde.push (+500);
+    console.log(compte);
+
+    let data = {
+    "id": "0001",
+    "type": "donut",
+    "name": "Cake",
+    "ppu": 0.55,
+    "batters":
+    {
+        "batter":
+            [
+                { "id": "1001", "type": "Rick" },
+                { "id": "1002", "type": "Chocolate" },
+                { "id": "1003", "type": "Blueberry" },
+                { "id": "1004", "type": "Devil's Food" }
+            ]
+    },
+    "topping":
+        [
+            { "id": "5001", "type": "None" },
+            { "id": "5002", "type": "Glazed" },
+            { "id": "5005", "type": "Morty" },
+            { "id": "5007", "type": "helloWorld" },
+            { "id": "5006", "type": "Chocolate with Sprinkles" },
+            { "id": "5003", "type": "Chocolate" },
+            { "id": "5004", "type": "Maple" }
+        ]
+}
+
+console.log(data.name);
+console.log(data.ppu);
+console.log(data["topping"]);
+console.log(data.batters.batter[3].type);
+console.log(data.topping[6].type);
+console.log(data.topping[0].id);
+
+let chemin1=(data.batters.batter[0].id);
+let nom=(data.batters.batter[0].type);
+let phrase=nom+ " est l'id "+chemin1;
+console.log(phrase);
+
+let a1=(data.topping[6].type);
+let phrase2= "cake au "+a1;
+console.log(phrase2);*/
+
+let personne = {
+	nom: 'alice',
+	age: 25,
+	ville: 'Paris' 
+	};
+    console.log(personne);
+
+    let compte = {
+        solde: 1000,
+        titulaire: 'john doe',
+    };
+    compte.solde+=500;
+    console.log(compte);
+
+    
