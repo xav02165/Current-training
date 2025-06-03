3fd6fbda56f17af95e2f37b8ac62a5ea0a35896b
xavier <xav.dx02@gmail.com>
2025-06-03 13:52:28 +0000

    compteur

--- programming/javascript/Morpion-projet/js.js
+++ programming/javascript/Morpion-projet/js.js
@@ -1,9 +1,7 @@
 /*const cellule = document.querySelectorAll(."case");
 
-const victoire [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]  ];
 
-let Reinitialiser = document.getElementById("Reinitialiser");
-let remise a zero = document.getElementById("remise a zero");
+
 let Resultat = document.getElementById("");
 let scoreX = 0;
 let scoreO = 0;
@@ -274,3 +272,4 @@ function verifVictoire(){
     }
 
 }
+
